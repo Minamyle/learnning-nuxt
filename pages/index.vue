cf44c9e482af15462ee3c79c2e98839be4f2a613
Amina Ismaila <minamyle@gmail.com>
2024-12-21 17:02:31 +0100

    feat: added navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,32 @@
 <template>
-    <div>
-<h2>Home</h2>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus omnis vero et laborum incidunt repudiandae sequi eius deserunt accusantium voluptatum.</p>
-    </div>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/"> Nuxt Dojo </NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus omnis
+      vero et laborum incidunt repudiandae sequi eius deserunt accusantium
+      voluptatum.
+    </p>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
-h2{
-    margin-bottom: 20px;
-    font-size: 36px;
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
 }
-p{
-    margin: 20px 0;
+p {
+  margin: 20px 0;
 }
-
 </style>
